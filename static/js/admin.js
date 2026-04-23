61976cd0f06a0330745538500fd0ab975aa5f15a
Annika Needham <annikaneedham@outlook.com>
2026-04-22 20:26:53 -0400

    added update book for admin

--- static/js/admin.js
+++ static/js/admin.js
@@ -39,3 +39,40 @@ document.getElementById("addBookForm").addEventListener("submit", function(f){
         alert("Error occurred.");
     });
 });
+document.getElementById("updateBookForm").addEventListener("submit", function(f){
+
+    f.preventDefault();
+
+     const info ={
+    
+        isbn: document.getElementById("u_isbn").value,
+        title: document.getElementById("u_title").value,
+        author: document.getElementById("u_author").value,
+        genre: document.getElementById("u_genre").value,
+        language: document.getElementById("u_language").value,
+        pages: parseInt(document.getElementById("u_pages").value),
+        date: document.getElementById("u_date").value,
+    };
+
+    fetch("/update_book", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(info)
+    })
+
+    .then(response => response.json())
+    .then(data => {
+        if (data.message) {
+            alert("Book updated");
+            document.getElementById("updateBookForm").reset();
+        } else if (data.error) {
+            alert("Error: " + data.error);
+        }
+    })
+
+    .catch(error => {
+        console.error("Error:", error);
+        alert("Error occurred");
+    });
+
+});
